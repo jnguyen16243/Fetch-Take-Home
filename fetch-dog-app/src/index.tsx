ba20f1f1fad385851a0031aa0a4dc80232ebcd7c
Joe Nguyen <joe.duc.minh.nguyen@gmail.com>
2025-02-25 17:48:55 -0700

    and env files for local and prodcution

--- fetch-dog-app/src/index.tsx
+++ fetch-dog-app/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+console.log("API URL:", process.env.REACT_APP_API_URL);
+console.log("Environment:", process.env.NODE_ENV);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
